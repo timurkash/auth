0ec62e3fe337c24938de9e9765015e525e7ca876
Timur Kash <timurkash@gmail.com>
2023-05-04 20:55:21 +0300

    next

--- assets/common/common.js
+++ assets/common/common.js
@@ -5,17 +5,16 @@ export function randomString(characters) {
   let result = '';
   let charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() *
-      charactersLength));
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result
 }
 
 export function getDateTime(ts) {
-  return new Date(ts.getSeconds() * 1000 + ts.getNanos()/1000000)
+  return new Date(ts.getSeconds() * 1000 + ts.getNanos() / 1000000)
 }
 
 export function getDateTimeString(ts) {
-  return moment(new Date(ts.getSeconds() * 1000 + ts.getNanos()/1000000))
+  return moment(new Date(ts.getSeconds() * 1000 + ts.getNanos() / 1000000))
     .format("yyyy-MM-DD HH:mm:ss.SSS")
 }
